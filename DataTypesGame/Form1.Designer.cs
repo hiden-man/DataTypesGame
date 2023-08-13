eaac03a6d809f19ddc2627feada92f365e443051
hiden-man <elderlutiquie@gmail.com>
2023-08-13 16:52:28 +0300

    Add project files.

---
+++ DataTypesGame/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace DataTypesGame
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numberRightAnswerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(71, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(486, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "****** Data Types Game ******\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(-6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(641, 38);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "----------------------------------------------------";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(121, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(489, 45);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(18, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 38);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Type";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.ForeColor = System.Drawing.Color.Lime;
+            this.radioButton1.Location = new System.Drawing.Point(49, 210);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(96, 40);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "byte";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.ForeColor = System.Drawing.Color.Lime;
+            this.radioButton2.Location = new System.Drawing.Point(151, 210);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(108, 40);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "short";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.ForeColor = System.Drawing.Color.Lime;
+            this.radioButton3.Location = new System.Drawing.Point(265, 210);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(71, 40);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "int";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton4.ForeColor = System.Drawing.Color.Lime;
+            this.radioButton4.Location = new System.Drawing.Point(342, 210);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(98, 40);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "long";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton5.ForeColor = System.Drawing.Color.Lime;
+            this.radioButton5.Location = new System.Drawing.Point(446, 210);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(133, 40);
+            this.radioButton5.TabIndex = 3;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "double";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton6.ForeColor = System.Drawing.Color.Lime;
+            this.radioButton6.Location = new System.Drawing.Point(157, 256);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(97, 40);
+            this.radioButton6.TabIndex = 3;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "float";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton7.ForeColor = System.Drawing.Color.Lime;
+            this.radioButton7.Location = new System.Drawing.Point(260, 256);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(116, 40);
+            this.radioButton7.TabIndex = 3;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "string";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton8.ForeColor = System.Drawing.Color.Lime;
+            this.radioButton8.Location = new System.Drawing.Point(382, 256);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(98, 40);
+            this.radioButton8.TabIndex = 3;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "char";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(255, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(323, 66);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "answer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numberRightAnswerLabel
+            // 
+            this.numberRightAnswerLabel.AutoSize = true;
+            this.numberRightAnswerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.numberRightAnswerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberRightAnswerLabel.ForeColor = System.Drawing.Color.Yellow;
+            this.numberRightAnswerLabel.Location = new System.Drawing.Point(29, 325);
+            this.numberRightAnswerLabel.Name = "numberRightAnswerLabel";
+            this.numberRightAnswerLabel.Size = new System.Drawing.Size(220, 76);
+            this.numberRightAnswerLabel.TabIndex = 0;
+            this.numberRightAnswerLabel.Text = "Right answer\r\n0\r\n";
+            this.numberRightAnswerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(628, 474);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numberRightAnswerLabel);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DataTypesGame";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label numberRightAnswerLabel;
+    }
+}
+
---
+++ DataTypesGame/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DataTypesGame
+{
+    public partial class MainForm : Form
+    {
+        Random random = new Random();
+        byte r;
+        byte type1 = 200;
+        short type2 = 19439;
+        int type3 = 293494220, numberRightAnswer = 0;
+        long type4 = 6060633343533333333;
+        double type5 = 3443000533.54344304050545;
+        float type6 = 6.2f;
+        string type7 = "I`m Superman";
+        char type8 = 'S';
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void RandomQuestion()
+        {
+            r = Convert.ToByte(random.Next(1, 9));
+            if (r == 1)
+                textBox1.Text = type1.ToString();
+            if (r == 2)
+                textBox1.Text = type2.ToString();
+            if (r == 3)
+                textBox1.Text = type3.ToString();
+            if (r == 4)
+                textBox1.Text = type4.ToString();
+            if (r == 5)
+                textBox1.Text = type5.ToString();
+            if (r == 6)
+                textBox1.Text = type6.ToString();
+            if (r == 7)
+                textBox1.Text = type7.ToString();
+            if (r == 8)
+                textBox1.Text = type8.ToString();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            RandomQuestion();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked && r == 1)
+            {
+                numberRightAnswer += 1;
+                numberRightAnswerLabel.Text = $"Right answer\n{numberRightAnswer}";
+            }
+            if (radioButton2.Checked && r == 2)
+            {
+                numberRightAnswer += 1;
+                numberRightAnswerLabel.Text = $"Right answer\n{numberRightAnswer}";
+            }
+            if (radioButton3.Checked && r == 3)
+            {
+                numberRightAnswer += 1;
+                numberRightAnswerLabel.Text = $"Right answer\n{numberRightAnswer}";
+            }
+            if (radioButton4.Checked && r == 4)
+            {
+                numberRightAnswer += 1;
+                numberRightAnswerLabel.Text = $"Right answer\n{numberRightAnswer}";
+            }
+            if (radioButton5.Checked && r == 5)
+            {
+                numberRightAnswer += 1;
+                numberRightAnswerLabel.Text = $"Right answer\n{numberRightAnswer}";
+            }
+            if (radioButton6.Checked && r == 6)
+            {
+                numberRightAnswer += 1;
+                numberRightAnswerLabel.Text = $"Right answer\n{numberRightAnswer}";
+            }
+            if (radioButton7.Checked && r == 7)
+            {
+                numberRightAnswer += 1;
+                numberRightAnswerLabel.Text = $"Right answer\n{numberRightAnswer}";
+            }
+            if (radioButton8.Checked && r == 8)
+            {
+                numberRightAnswer += 1;
+                numberRightAnswerLabel.Text = $"Right answer\n{numberRightAnswer}";
+            }
+            RandomQuestion();
+        }
+    }
+}
